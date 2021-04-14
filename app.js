a5d4d638707620f3fb2a96812e0cf33a5f7f0bbe
Ryan Park <ryan.developer99@gmail.com>
2021-04-14 20:35:23 +0900

    Second commit: Synchronous experiment

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+console.log("I'm first");
+console.log("I'm second");
+console.log("I'm third");
+
+console.log("I'm first");
+boilingWater();
+console.log("I'm third");
+
+function boilingWater() {
+  console.log("boiling...");
+  for (i = 0; i < 1000; i++) {
+    console.log("still not done...");
+  }
+  console.log("Done.");
+}
